c17892820f27ede5494fc714eb5d69c4e002e86f
Timothy <timmygroup1@gmail.com>
2022-10-13 07:11:18 +0000

    improved mobile navigation.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-lists');
+const navLink = document.querySelectorAll('.nav-link');
 
 const mobileToggle = () => {
     hamburger.classList.toggle('active');
     navList.classList.toggle('active');
 
 }
-
 hamburger.addEventListener('click', mobileToggle)
+navLink.forEach(link => link.addEventListener('click',mobileToggle ))
